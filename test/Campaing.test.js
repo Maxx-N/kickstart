bc38c8f2ed09a64b3bb92ca29f1f6437df04aa12
Maxime Norval <maxime.norval@royalcanin.com>
2021-07-14 16:43:56 +0200

    152. Array Getters

--- test/Campaing.test.js
+++ test/Campaing.test.js
@@ -64,4 +64,14 @@ describe('Campaigns', () => {
 
     assert(isErrorThrown);
   });
+
+  it('allows a manager to make a payment request', async () => {
+    await campaign.methods
+      .createRequest('Buy batteries.', '100', accounts[1])
+      .send({ from: accounts[0], gas: '1000000' });
+
+    const request = await campaign.methods.requests(0).call();
+
+    assert.equal(request.description, 'Buy batteries.');
+  });
 });
