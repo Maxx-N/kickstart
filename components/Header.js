9d5a4f2d66ab96c0ac1c5f658bd2546bb335122b
Maxime Norval <maxime.norval@royalcanin.com>
2021-07-14 22:52:59 +0200

    180. Constraining Content Width

--- components/Header.js
+++ components/Header.js
@@ -1,14 +1,13 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Menu } from 'semantic-ui-react';
 
 const Header = () => {
   return (
-    <Menu>
+    <Menu style={{ marginTop: '10px' }}>
       <Menu.Item>CrowdCoin</Menu.Item>
 
       <Menu.Menu position='right'>
         <Menu.Item>Campaigns</Menu.Item>
-
         <Menu.Item>+</Menu.Item>
       </Menu.Menu>
     </Menu>
