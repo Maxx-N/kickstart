eb0129a44433738820b7475784f21e5aa2ddcd9b
Maxime Norval <maxime.norval@royalcanin.com>
2021-07-15 19:11:54 +0200

    188. Form Error Handling

--- pages/campaigns/new.js
+++ pages/campaigns/new.js
@@ -1,26 +1,32 @@
-import React, { Component } from 'react'
-import { Button, Form, Input } from 'semantic-ui-react'
-import Layout from '../../components/Layout'
-import factory from '../../ethereum/factory'
-import web3 from '../../ethereum/web3'
+import React, { Component } from 'react';
+import { Button, Form, Input, Message } from 'semantic-ui-react';
+import Layout from '../../components/Layout';
+import factory from '../../ethereum/factory';
+import web3 from '../../ethereum/web3';
 
 class CampaignNew extends Component {
-  state = { minimumContribution: '' }
+  state = { minimumContribution: '', errorMessage: '' };
 
-  onSubmit = async event => {
-    event.preventDefault()
+  onSubmit = async (event) => {
+    event.preventDefault();
 
-    const accounts = await web3.eth.getAccounts()
+    try {
+      const accounts = await web3.eth.getAccounts();
 
-    await factory.methods.createCampaign(this.state.minimumContribution).send({
-      from: accounts[0]
-    })
-  }
+      await factory.methods
+        .createCampaign(this.state.minimumContribution)
+        .send({
+          from: accounts[0],
+        });
+    } catch (err) {
+      this.setState({ errorMessage: err.message });
+    }
+  };
 
-  render () {
+  render() {
     return (
       <Layout>
-        <Form onSubmit={this.onSubmit}>
+        <Form onSubmit={this.onSubmit} error={!!this.state.errorMessage}>
           <h3>Create a Campaign</h3>
           <Form.Field>
             <label>Minimum Contribution</label>
@@ -28,16 +34,17 @@ class CampaignNew extends Component {
               label='wei'
               labelPosition='right'
               value={this.state.minimumContribution}
-              onChange={event => {
-                this.setState({ minimumContribution: event.target.value })
+              onChange={(event) => {
+                this.setState({ minimumContribution: event.target.value });
               }}
             />
           </Form.Field>
+          <Message error header='Oops!' content={this.state.errorMessage} />
           <Button primary>Create!</Button>
         </Form>
       </Layout>
-    )
+    );
   }
 }
 
-export default CampaignNew
+export default CampaignNew;
